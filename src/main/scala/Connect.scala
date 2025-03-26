d019ffd27bfd7f9aa8b5047f1c11cc46411419d0
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-03-26 15:52:32 +0100

    in progress

--- src/main/scala/Connect.scala
+++ src/main/scala/Connect.scala
@@ -18,32 +18,33 @@ extension (p: Pos)
       case Direction.W   => Pos(p.x, p.y - 1)
 
 class Connect(board: List[String]):
-   private val (maxX, maxY) = (board.head.length, board.length)
-   board.zipWithIndex.map((l, i) => " " * i + l).foreach(println)
-   println("-" * 20)
-   board.foreach(println)
-   println("-" * 20)
-
-   private def charAtPos(p: Pos): Option[Char] =
-      if p.x < 0 || p.x > maxX - 1 || p.y < 0 || p.y > maxY - 1 then None else Some(board(p.x)(p.y))
-
+   private val (maxX, maxY) = (board.head.indices.last, board.indices.last)
+   // board.zipWithIndex.map((l, i) => " " * i + l).foreach(println)
+   // println("-" * 20)
+   // board.foreach(println)
+   // println("-" * 20)
    private val startRowBlack: Int = board.map(_.head).indexOf('X')
    private val startColWhite: Int = board.head.indexOf('O')
    private val startPosBlack: Option[Pos] = if startRowBlack == -1 then None else Some(Pos(startRowBlack, 0))
    private val startPosWhite: Option[Pos] = if startColWhite == -1 then None else Some(Pos(0, startColWhite))
 
-   println(s"Black at $startPosBlack, White at $startPosWhite")
+   // println(s"Black at $startPosBlack, White at $startPosWhite")
 
-   private val possibleNext: Array[Option[Char]] =
-      if startPosBlack.isDefined then for d <- Direction.values yield charAtPos(startPosBlack.get.vicinity(d))
-      else Array(None)
-   println(possibleNext.toList)
+   private def charAtPos(p: Pos): Option[Char] = Option(board(p.x)(p.y))
+
+   private def validNeighbours(c: Char, p: Pos): Seq[Pos] =
+      (for d <- Direction.values yield p.vicinity(d))
+         .filterNot(p => p.x < 0)
+         .filterNot(p => p.x > maxX)
+         .filterNot(p => p.y < 0)
+         .filterNot(p => p.y > maxY)
+         .filter(p => charAtPos(p).get == c)
+
+   println(validNeighbours('X', startPosBlack.getOrElse(Pos(-1, -1))))
 
    def winner: Option[Color] =
-
-      def whiteWin = board.size == 1 & startPosWhite.isDefined
-      def blackWin = board.size == 1 & startPosBlack.isDefined
-
+      val whiteWin = board.size == 1 & startPosWhite.isDefined
+      val blackWin = board.size == 1 & startPosBlack.isDefined
       (whiteWin, blackWin) match
          case (true, false) => Some(Color.White)
          case (false, true) => Some(Color.Black)
