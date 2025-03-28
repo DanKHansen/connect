3afdf646c52eba6a3a7c2c56e13cf935d4d0e7e3
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-03-28 15:52:08 +0100

    in progress

--- src/main/scala/Connect.scala
+++ src/main/scala/Connect.scala
@@ -1,51 +1,74 @@
 import Direction.*
+import scala.annotation.tailrec
+import scala.util.{Failure, Success, Try}
 
 enum Color:
    case Black, White
 enum Direction:
    case N, NE, NEE, E, S, SW, SWW, W
 
-case class Pos(x: Int, y: Int)
+case class Pos(row: Int, col: Int)
 extension (p: Pos)
    private def vicinity(d: Direction): Pos = d match
-      case Direction.N   => Pos(p.x - 1, p.y)
-      case Direction.NE  => Pos(p.x - 1, p.y + 1)
-      case Direction.NEE => Pos(p.x - 1, p.y + 2)
-      case Direction.E   => Pos(p.x, p.y + 1)
-      case Direction.S   => Pos(p.x + 1, p.y)
-      case Direction.SW  => Pos(p.x + 1, p.y - 1)
-      case Direction.SWW => Pos(p.x + 1, p.y - 2)
-      case Direction.W   => Pos(p.x, p.y - 1)
+      case Direction.N   => Pos(p.row - 1, p.col)
+      case Direction.NE  => Pos(p.row - 1, p.col + 1)
+      case Direction.NEE => Pos(p.row - 1, p.col + 2)
+      case Direction.E   => Pos(p.row, p.col + 1)
+      case Direction.S   => Pos(p.row + 1, p.col)
+      case Direction.SW  => Pos(p.row + 1, p.col - 1)
+      case Direction.SWW => Pos(p.row + 1, p.col - 2)
+      case Direction.W   => Pos(p.row, p.col - 1)
 
 class Connect(board: List[String]):
-   private val (maxX, maxY) = (board.head.indices.last, board.indices.last)
-   // board.zipWithIndex.map((l, i) => " " * i + l).foreach(println)
-   // println("-" * 20)
-   // board.foreach(println)
-   // println("-" * 20)
-   private val startRowBlack: Int = board.map(_.head).indexOf('X')
-   private val startColWhite: Int = board.head.indexOf('O')
-   private val startPosBlack: Option[Pos] = if startRowBlack == -1 then None else Some(Pos(startRowBlack, 0))
-   private val startPosWhite: Option[Pos] = if startColWhite == -1 then None else Some(Pos(0, startColWhite))
+   private val (maxRowIdx, maxColIdx) = (board.indices.last, board.head.indices.last)
+   board.zipWithIndex.map((l, i) => " " * i + l).foreach(println)
+   println("-" * 20)
+   board.foreach(println)
+   println("-" * 20)
 
-   // println(s"Black at $startPosBlack, White at $startPosWhite")
+   private val startRowBlack: List[Int] = findIndices(board.map(_.head), _ == 'X')
+   private val startColWhite: List[Int] = findIndices(board.head.toList, _ == 'O')
+   private val startPosBlack: List[Pos] = if startRowBlack.isEmpty then Nil else startRowBlack.map(Pos(_, 0))
+   private val startPosWhite: List[Pos] = if startColWhite.isEmpty then Nil else startColWhite.map(Pos(0, _))
 
-   private def charAtPos(p: Pos): Option[Char] = Option(board(p.x)(p.y))
+   println(s"Black at $startPosBlack, White at $startPosWhite")
+   println(s"max RowIndex: $maxRowIdx, max ColIndex: $maxColIdx")
 
-   private def validNeighbours(c: Char, p: Pos): Seq[Pos] =
-      (for d <- Direction.values yield p.vicinity(d))
-         .filterNot(p => p.x < 0)
-         .filterNot(p => p.x > maxX)
-         .filterNot(p => p.y < 0)
-         .filterNot(p => p.y > maxY)
-         .filter(p => charAtPos(p).get == c)
+   private def findIndices[T](list: List[T], predicate: T => Boolean): List[Int] =
+      list.zipWithIndex.collect {
+         case (element, index) if predicate(element) => index
+      }
 
-   println(validNeighbours('X', startPosBlack.getOrElse(Pos(-1, -1))))
+   private def charAtPos(p: Pos): Char = board(p.row)(p.col)
+
+   // tailrec
+   private def checkPath(c: Char, ps: List[Pos], visited: List[Pos]): Boolean =
+      val nextPos = (for d <- Direction.values yield ps.head.vicinity(d))
+         .filterNot(_.row < 0)
+         .filterNot(_.col < 0)
+         .filterNot(_.row > maxRowIdx)
+         .filterNot(_.col > maxColIdx)
+         .filter(p => charAtPos(p) == c)
+
+      if nextPos.isEmpty then false
+      else
+         println(nextPos.toSeq)
+         false
+      // checkPath(c, nextPos.toList, ps.head :: visited)
 
    def winner: Option[Color] =
-      val whiteWin = board.size == 1 & startPosWhite.isDefined
-      val blackWin = board.size == 1 & startPosBlack.isDefined
-      (whiteWin, blackWin) match
-         case (true, false) => Some(Color.White)
-         case (false, true) => Some(Color.Black)
+      val midBordEmpty = board.nonEmpty && board.tail.nonEmpty && board.tail.init.forall(_.tail.init.forall(_ == '.'))
+      val singularBoard = board.size == 1
+      val checkWhite =
+         if startPosWhite.isEmpty || midBordEmpty then false
+         else if singularBoard && startPosWhite.nonEmpty then true
+         else checkPath('O', startPosWhite, Nil)
+      val checkBlack =
+         if startPosBlack.isEmpty || midBordEmpty then false
+         else if singularBoard && startPosBlack.nonEmpty then true
+         else checkPath('X', startPosBlack, Nil)
+
+      (checkBlack, checkWhite) match
+         case (false, true) => Some(Color.White)
+         case (true, false) => Some(Color.Black)
          case _             => None
